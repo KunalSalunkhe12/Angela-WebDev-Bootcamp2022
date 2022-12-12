a0049b6d7be1ae766f041098e91b0bcfac205550
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2022-12-12 16:41:48 +0530

    Refactor WeatherApp-(express, OpenweatherApi)

--- WeatherApp-(express, OpenweatherApi)/app.js
+++ WeatherApp-(express, OpenweatherApi)/app.js
@@ -1,32 +1,26 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
-const { json } = require("body-parser");
-
-
+const { dirname } = require("path");
 
 const app = express();
 
-app.set('view engine' , 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'))
 
-let temp = undefined;
-let weatherDescription = undefined;
-let weatherUrl = undefined;
 
 app.get("/" , (req , res)=>{
 
-  res.render('index' , {temp: temp , weatherDescription: weatherDescription});
+  res.sendFile(__dirname + '/index.html')
 
 });
 
 app.post("/", (req,res)=>{
 
   let query = req.body.cityName;
+  let units = req.body.unitName
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=7c9f793334e3f266caa6b5d787378022&units=Metrics";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=7c9f793334e3f266caa6b5d787378022&units=" + units;
   https.get(url , (response)=>{
     console.log(response.statusCode);
 
@@ -39,16 +33,14 @@ app.post("/", (req,res)=>{
       let weatherIcon = weatherData.weather[0].icon;
       weatherUrl = "http://openweathermap.org/img/wn/"+ weatherIcon + "@2x.png"
 
-      res.render('index' , {temp: temp , weatherDescription: weatherDescription, weatherUrl: weatherUrl});
-      res.redirect('/')
-      // if(units == "Metric" ){
-      //     res.write("<h1>The temperature in "+ query + " is: " + temp + " Degrees celcius.</h1>")
-      // }else{
-      //     res.write("<h1>The temperature in "+ query + " is: " + temp + " Fahrenheit.</h1>")
-      // }
+      if(units == "Metric" ){
+          res.write("<h1>The temperature in "+ query + " is: " + temp + " Degrees celcius.</h1>")
+      }else{
+          res.write("<h1>The temperature in "+ query + " is: " + temp + " Fahrenheit.</h1>")
+      }
 
-      // res.write("<p> Weather Description: " + weatherDescription + "</p>")
-      // res.write("<img src=" + weatherUrl + ">");
+      res.write("<p> Weather Description: " + weatherDescription + "</p>")
+      res.write("<img src=" + weatherUrl + ">");
      
     })
   })
